6bebc805aa10ae7fc5de2fce6e1f70ac6ce2db44
minsun0930 <203045311+minsun0930@users.noreply.github.com>
2026-09-19 02:27:00 +0900

    feat: 스켈레톤 UI 추가 및 하트 아이콘(찜 버튼) 추가

--- src/components/movie/MovieCardSkeleton.tsx
+++ src/components/movie/MovieCardSkeleton.tsx
@@ -1,6 +1,6 @@
 export default function MovieCardSkeleton() {
   return (
-    <article className="relative flex shrink-0 w-45 animate-pulse ">
+    <article className="relative flex shrink-0 w-45 animate-pulse">
       <div className="w-full aspect-2/3 bg-gray-700/50 rounded-xl" />
     </article>
   );
